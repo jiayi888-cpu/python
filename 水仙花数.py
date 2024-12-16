6efb453202cfb4b73266db9a43ee58bd996b66a8
jiayi888-cpu <3332518650@qq.com>
2024-12-16 17:40:31 +0800

    Add files via upload

---
+++ 水仙花数.py
@@ -0,0 +1,9 @@
+for a in range(1,10):
+    for b in range(0,10):
+        for c in range(0,10):
+            s1=a*100+b*10+c
+            s2=a**3+b**3+c**3
+            if s1==s2:
+                print(s1)
+        
+    
